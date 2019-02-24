84764ab552e070f9502f0af6e4a91535312a956f
roctubre <robertryan.chavez@gmail.com>
2019-02-25 00:36:53 +0100

    removed random transformation for validation and test data

--- model.py
+++ model.py
@@ -177,9 +177,16 @@ class Network(object):
         test_dir = data_dir + '/test'
         
         # Define your transforms for the training, validation, and testing sets
-        data_transforms = transforms.Compose([transforms.RandomRotation(30),
+        traindata_transforms = transforms.Compose([transforms.RandomRotation(30),
                                               transforms.RandomResizedCrop(224),
                                               transforms.RandomHorizontalFlip(),
+                                              transforms.ToTensor(),
+                                              transforms.Normalize(
+                                                    (0.485, 0.456, 0.406),
+                                                    (0.229, 0.224, 0.225))
+                                              ])
+        
+        data_transforms = transforms.Compose([transforms.RandomResizedCrop(224),
                                               transforms.ToTensor(),
                                               transforms.Normalize(
                                                     (0.485, 0.456, 0.406),
@@ -188,7 +195,7 @@ class Network(object):
 
         # Load the datasets with ImageFolder and save class_to_idx
         image_datasets = {
-            "train": datasets.ImageFolder(train_dir, transform=data_transforms),
+            "train": datasets.ImageFolder(train_dir, transform=traindata_transforms),
             "valid": datasets.ImageFolder(valid_dir, transform=data_transforms),
             "test": datasets.ImageFolder(test_dir, transform=data_transforms)
             }
